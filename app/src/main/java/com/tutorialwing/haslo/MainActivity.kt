26a26326e75db881ba28ec4a3772d6498d48ca3a
OGMaksiu <mateckimaksymilian@gmail.com>
2023-01-30 15:58:32 +0100

    Poprawki aby wypisywało na ekranie

--- app/src/main/java/com/tutorialwing/haslo/MainActivity.kt
+++ app/src/main/java/com/tutorialwing/haslo/MainActivity.kt
@@ -4,53 +4,58 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var passwordTextView: TextView
     private lateinit var passwordEditText: EditText
-    private lateinit var passwordAgainEditText: EditText
+    private lateinit var repeatPasswordEditText: EditText
     private lateinit var checkButton: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         passwordEditText = findViewById(R.id.editTextTextPassword)
-        passwordAgainEditText = findViewById(R.id.editTextTextPassword2)
+        repeatPasswordEditText = findViewById(R.id.editTextTextPassword2)
         checkButton = findViewById(R.id.button)
+        passwordTextView = findViewById(R.id.textView)
 
         checkButton.setOnClickListener {
             val password = passwordEditText.text.toString()
-            val passwordAgain = passwordAgainEditText.text.toString()
-            if (password == passwordAgain) {
-                if (checkPassword(password)) {
-                    Toast.makeText(this, "Hasło jest poprawne", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(this, "Hasło musi zawierać co najmniej jedną wielką literę, jedną małą literę, jedną cyfrę i jeden znak specjalny", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                Toast.makeText(this, "Hasła nie są zgodne", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
+            val repeatPassword = repeatPasswordEditText.text.toString()
+            var isValid = true
 
-    private fun checkPassword(password: String): Boolean {
-        var hasUpper = false
-        var hasLower = false
-        var hasDigit = false
-        var hasSpecial = false
-        for (char in password) {
-            if (char.isUpperCase()) {
-                hasUpper = true
-            } else if (char.isLowerCase()) {
-                hasLower = true
-            } else if (char.isDigit()) {
-                hasDigit = true
-            } else if (!char.isLetterOrDigit()) {
-                hasSpecial = true
+            if (password != repeatPassword) {
+                passwordTextView.text = "Hasła nie są takie same."
+                isValid = false
+            }
+
+            if (!password.matches(".*[A-Z].*".toRegex())) {
+                passwordTextView.text = "${passwordTextView.text}\nHasło powinno zawierać przynajmniej jedną wielką literę."
+                isValid = false
+            }
+
+            if (!password.matches(".*[a-z].*".toRegex())) {
+                passwordTextView.text = "${passwordTextView.text}\nHasło powinno zawierać przynajmniej jedną małą literę."
+                isValid = false
+            }
+
+            if (!password.matches(".*\\d.*".toRegex())) {
+                passwordTextView.text = "${passwordTextView.text}\nHasło powinno zawierać przynajmniej jedną cyfrę."
+                isValid = false
+            }
+
+            if (!password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?].*".toRegex())) {
+                passwordTextView.text = "${passwordTextView.text}\nHasło powinno zawierać przynajmniej jeden znak specjalny."
+                isValid = false
+            }   Dao
+
+            if (isValid) {
+                passwordTextView.text = "Hasło jest prawidłowe."
             }
         }
-        return hasUpper && hasLower && hasDigit && hasSpecial
     }
 }
